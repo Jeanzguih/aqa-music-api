314e11e9d39f4b56e2732408258ef921f9312324
Jeanzguih <jeanpla2003@gmail.com>
2025-06-06 21:05:44 -0300

    feat(app):inicialização do banco

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express = require('express');
+const { AppDataSource } = require('./config/data-source');
 const app = express()
 
 app.use(express.json())
+
+AppDataSource.initialize()
+    .then(() => console.log("📦Database ready!"))
+    .catch((err) => console.error(err));
 
 module.exports = app
