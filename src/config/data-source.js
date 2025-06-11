11978392592613f8f778ea1674fa0ae4d82bd2cf
Jeanzguih <jeanpla2003@gmail.com>
2025-06-10 21:32:38 -0300

    feat(config datasource): Adicionado tabela media ao banco

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -1,11 +1,12 @@
 const { DataSource } = require("typeorm");
 
+const Media = require("../entities/media");
 
 const AppDataSource = new DataSource({
 type: "sqlite",
 database: "./src/database/aqa-music-database.sqlite",
 synchronize: true,
-entities: [],
+entities: [Media],
 });
 
 
