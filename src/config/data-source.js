d248c309496edee555cf15156d4cce40a221c40a
Leolekk <oliveira.leo1407@gmail.com>
2025-06-10 21:30:59 -0300

    feat (config data-source): Adicionado tabela do usuário ao banco

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -1,11 +1,12 @@
 const { DataSource } = require("typeorm");
 
+const User = require("../entities/User");
 
 const AppDataSource = new DataSource({
-type: "sqlite",
-database: "./src/database/aqa-music-database.sqlite",
-synchronize: true,
-entities: [],
+    type: "sqlite",
+    database: "./src/database/aqa-music-database.sqlite",
+    synchronize: true,
+    entities: [User],
 });
 
 
