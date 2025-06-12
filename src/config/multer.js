7cfc93bdf52c9ee2093f6d620f5af4a77f28a939
Jeanzguih <jeanpla2003@gmail.com>
2025-06-11 21:32:37 -0300

    feat(multers): criado multers

---
+++ src/config/multer.js
@@ -0,0 +1,11 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: path.resolve(__dirname, "..", "uploads"),
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+module.exports = multer({ storage });
