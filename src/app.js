cdb6ecd67c9136bafc1e92ae4518d21748e0672e
Jeanzguih <jeanpla2003@gmail.com>
2025-06-11 21:31:46 -0300

    feat(app): emplementado rota ao app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { AppDataSource } = require('./config/data-source');
-const app = express()
+const routes = require("./routes");
 
+const app = express()
 app.use(express.json())
+
+app.use("/uploads", express.static("uploads"));
+app.use("/api", routes);
 
 AppDataSource.initialize()
     .then(() => console.log("📦Database ready!"))
