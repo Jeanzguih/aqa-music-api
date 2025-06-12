451dfe543838f10ae0df17156a5c50fd702535f0
Leolekk <oliveira.leo1407@gmail.com>
2025-06-11 21:32:10 -0300

    feat(routes api): emplementado rota de api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { AppDataSource } = require('./config/data-source');
+const routes = require('./routes');
 const app = express()
 
 app.use(express.json())
+app.use("/api", routes);
 
 AppDataSource.initialize()
     .then(() => console.log("📦Database ready!"))
