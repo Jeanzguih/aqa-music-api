965b0e5a6a513287e1843f4271a74e43b40f76d8
Jeanzguih <jeanpla2003@gmail.com>
2025-06-12 20:34:50 -0300

    Update users.controllers.js

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,23 +1,23 @@
 const UsersService = require("../services/users.services")
 
 module.exports = {
-     createUser: async (request, response ) => {
-        try{
-        const {phone, nickname, name, password, email} = request.body
+    createUser: async (request, response) => {
+        try {
+            const { phone, nickname, name, password, email } = request.body
 
-        const user = await UsersService.createUser({
-            phone,
-            nickname,
-            name,
-            password,
-            email,
-        })
+            const user = await UsersService.createUser({
+                phone,
+                nickname,
+                name,
+                password,
+                email,
+            })
 
-        return response.json(user)
-    } catch (error) {
-        response.status(500).json({ message: error.message});
-    }
+            return response.json(user)
+        } catch (error) {
+            response.status(500).json({ message: error.message });
+        }
 
     },
-    authenticateUser: async (request, response ) => {},
+    authenticateUser: async (request, response) => { },
 }
