67d428a7c780d38276b683df3966ab039974dab9
Jeanzguih <jeanpla2003@gmail.com>
2025-06-12 21:02:30 -0300

    feat(multer): refector inplementa tratativa para multi arquivos

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const routes = require('./routes');
 
 const app = express()
 app.use(express.json())
-app.use("/api", routes);
 
 app.use("/uploads", express.static("uploads"));
 app.use("/api", routes);
