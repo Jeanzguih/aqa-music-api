2d7265a7154d6d4d0107173ed24b734419eb64b3
Jeanzguih <jeanpla2003@gmail.com>
2025-06-12 21:05:10 -0300

    feat(media.controller): implementado função para diferenciar audio de cover

--- src/controllers/media.controllers.js
+++ src/controllers/media.controllers.js
@@ -1,17 +1,29 @@
-const MediaService = require("../services/media.services");
+const MediaService = require("../services/media.services")
+const { AppDataSource } = require("../config/data-source")
+const mediaRepository = AppDataSource.getRepository('Media')
+
+const service = new MediaService(mediaRepository)
 
 module.exports = {
     createMedia: async (request, response) => {
         try {
             const { duration, description, title, genre } = request.body
 
-            const media = await MediaService.createMedia({
-                filePath: request.file.path,
-                imagePath: request.image.path,
+            const audioFile = request.files["audio"]?.[0]
+            const coverFile = request.files["cover"]?.[0]
+
+            if (!audioFile)
+                return response.status(400).json({ message: "Audio file is required." });
+
+            const media = await service.createMedia({
                 duration,
                 description,
                 title,
                 genre,
+                filePath: audioFile.path,
+                imagePath: coverFile.path,
+                updatedAt: new Date(),
+                createdByUserId: 1
             })
 
             return response.json(media)
